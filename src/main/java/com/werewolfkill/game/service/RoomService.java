b54db7aad88416eb72e921494c13b52a36367ee5
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2025-10-16 17:24:26 +0300

    missing import

--- src/main/java/com/werewolfkill/game/service/RoomService.java
+++ src/main/java/com/werewolfkill/game/service/RoomService.java
@@ -8,6 +8,7 @@ import com.werewolfkill.game.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
 import java.util.List;
 import java.util.UUID;
