814abaad35c4a1128707e60aad3f216ab63e96b6
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2025-10-22 17:13:36 +0300

    circular dependency fix

--- src/main/java/com/werewolfkill/game/service/GameService.java
+++ src/main/java/com/werewolfkill/game/service/GameService.java
@@ -8,6 +8,7 @@ import com.werewolfkill.game.session.SessionManager;
 import com.werewolfkill.game.session.SessionManager.PlayerInfo;
 import com.werewolfkill.game.session.SessionManager.RoomSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +32,7 @@ public class GameService {
     private SessionManager sessionManager;
 
     @Autowired
+    @Lazy
     private PhaseTimerService phaseTimerService;
 
     /**
